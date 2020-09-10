06be1bfadaac4006e00161c0bd48be1f9a2a516f
James Su-Shum <sushumjames@gmail.com>
2020-09-09 23:17:50 -0400

    Added basic ability for user interaction of Roommate data

--- Roommates/Program.cs
+++ Roommates/Program.cs
@@ -14,62 +14,66 @@ namespace Roommates
         private const string CONNECTION_STRING = @"server=localhost\SQLExpress;database=Roommates;integrated security=true";
         static void Main(string[] args)
         {
-            //RoomRepository roomRepo = new RoomRepository(CONNECTION_STRING);
+            // <----- Rooms Test Code ----->
 
-            //Console.WriteLine("Getting All Rooms:");
-            //Console.WriteLine();
+            /*RoomRepository roomRepo = new RoomRepository(CONNECTION_STRING);
 
-            //List<Room> allRooms = roomRepo.GetAll();
+            Console.WriteLine("Getting All Rooms:");
+            Console.WriteLine();
 
-            //foreach (Room room in allRooms)
-            //{
-            //    Console.WriteLine($"{room.Id} {room.Name} {room.MaxOccupancy}");
-            //}
+            List<Room> allRooms = roomRepo.GetAll();
 
-            //Console.WriteLine("----------------------------");
-            //Console.WriteLine("Getting Room with Id 1");
+            foreach (Room room in allRooms)
+            {
+                Console.WriteLine($"{room.Id} {room.Name} {room.MaxOccupancy}");
+            }
 
-            //Room singleRoom = roomRepo.GetById(1);
+            Console.WriteLine("----------------------------");
+            Console.WriteLine("Getting Room with Id 1");
 
-            //Console.WriteLine($"{singleRoom.Id} {singleRoom.Name} {singleRoom.MaxOccupancy}");
+            Room singleRoom = roomRepo.GetById(1);
 
-            //Room bathroom = new Room
-            //{
-            //    Name = "Bathroom",
-            //    MaxOccupancy = 1
-            //};
+            Console.WriteLine($"{singleRoom.Id} {singleRoom.Name} {singleRoom.MaxOccupancy}");
+
+            Room bathroom = new Room
+            {
+                Name = "Bathroom",
+                MaxOccupancy = 1
+            };
 
 
-            //roomRepo.Insert(bathroom);
+            roomRepo.Insert(bathroom);
 
-            //Console.WriteLine("-------------------------------");
-            //Console.WriteLine($"Added the new Room with id {bathroom.Id}");
+            Console.WriteLine("-------------------------------");
+            Console.WriteLine($"Added the new Room with id {bathroom.Id}");
 
-            //bathroom.MaxOccupancy = 2;
+            bathroom.MaxOccupancy = 2;
 
-            //roomRepo.Update(bathroom);
+            roomRepo.Update(bathroom);
 
-            //Console.WriteLine("-------------------------------");
+            Console.WriteLine("-------------------------------");
 
-            //allRooms = roomRepo.GetAll();
+            allRooms = roomRepo.GetAll();
 
-            //foreach (Room room in allRooms)
-            //{
-            //    Console.WriteLine($"{room.Id} {room.Name} {room.MaxOccupancy}");
-            //}
+            foreach (Room room in allRooms)
+            {
+                Console.WriteLine($"{room.Id} {room.Name} {room.MaxOccupancy}");
+            }
 
-            //roomRepo.Delete(8);
+            roomRepo.Delete(8);
 
-            //Console.WriteLine("-------------------------------");
+            Console.WriteLine("-------------------------------");
 
-            //allRooms = roomRepo.GetAll();
+            allRooms = roomRepo.GetAll();
 
-            //foreach (Room room in allRooms)
-            //{
-            //    Console.WriteLine($"{room.Id} {room.Name} {room.MaxOccupancy}");
-            //}
+            foreach (Room room in allRooms)
+            {
+                Console.WriteLine($"{room.Id} {room.Name} {room.MaxOccupancy}");
+            }*/
 
-            RoommateRepository roommateRepo = new RoommateRepository(CONNECTION_STRING);
+            // <----- Roommates Test Code ----->
+
+            /*RoommateRepository roommateRepo = new RoommateRepository(CONNECTION_STRING);
 
             Console.WriteLine("Getting All Roommates:");
             Console.WriteLine();
@@ -107,13 +111,60 @@ namespace Roommates
                 RoomId = 1
             };
 
-            /*roommateRepo.Insert(newRoommate);*/
+            roommateRepo.Insert(newRoommate);
 
             newRoommate.RentPortion = 10;
 
-            /*roommateRepo.Update(newRoommate);*/
+            roommateRepo.Update(newRoommate);
 
-            roommateRepo.Delete(5);
+            roommateRepo.Delete(5);*/
+
+            RoommateRepository roommateRepo = new RoommateRepository(CONNECTION_STRING);
+
+            Console.WriteLine("Welcome to Roommates!");
+            Console.WriteLine("Please select an option from the menu below:");
+            Console.WriteLine("(1) View all roommates (2) View specific roommate by Id (3) Add a new roommate ");
+
+            int userResponse = Int32.Parse(Console.ReadLine());
+
+            switch (userResponse)
+            {
+                case 1:
+                    List<Roommate> listOfRoommates = roommateRepo.GetAll();
+                    foreach (Roommate roommate in listOfRoommates)
+                    {
+                        Console.WriteLine($"Id: {roommate.Id} Name: {roommate.Firstname} {roommate.Lastname}");
+                    };
+                    break;
+                case 2:
+                    Console.Write("Enter a roommate Id: ");
+                    int res = Int32.Parse(Console.ReadLine());
+                    Roommate foundRoommate = roommateRepo.GetById(res);
+                    Console.WriteLine($"Name: {foundRoommate.Firstname} {foundRoommate.Lastname} Moved In Date: {foundRoommate.MovedInDate}");
+                    break;
+                case 3:
+                    Console.Write("Enter first name: ");
+                    string firstName = Console.ReadLine();
+                    Console.Write("Enter last name: ");
+                    string lastName = Console.ReadLine();
+                    Console.Write("Enter rent portion: ");
+                    int rentPortion = Int32.Parse(Console.ReadLine());
+                    Console.Write("Moved in date: ");
+                    DateTime movedInDate = DateTime.Parse(Console.ReadLine());
+                    Console.Write("Enter room id: ");
+                    int roomId = Int32.Parse(Console.ReadLine());
+                    Roommate newRoommate = new Roommate
+                    {
+                        Firstname = firstName,
+                        Lastname = lastName,
+                        RentPortion = rentPortion,
+                        MovedInDate = movedInDate,
+                        RoomId = roomId
+                    };
+                    roommateRepo.Insert(newRoommate);
+                    Console.WriteLine($"{firstName} {lastName} was successfully added!");
+                    break;
+            }
         }
     }
 }
